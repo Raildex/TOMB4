8f64139ff9dadeae93a96625ed982e313d07a5b1
Raildex <n@a.com>
2024-06-29 10:19:43 +0200

    moved SPHERE definition before array declaration

--- TOMB4/game/sphere.h
+++ TOMB4/game/sphere.h
@@ -1,8 +1,14 @@
-#ifndef SPHERE_INCLUDED
-#define SPHERE_INCLUDED
+#ifndef TOMB4_GAME_SPHERE_H
+#define TOMB4_GAME_SPHERE_H
+
+typedef struct SPHERE {
+	long x;
+	long y;
+	long z;
+	long r;
+} SPHERE;
 typedef struct ITEM_INFO ITEM_INFO;
 typedef struct PHD_VECTOR PHD_VECTOR;
-typedef struct SPHERE SPHERE;
 long GetSpheres(ITEM_INFO* item, SPHERE* ptr, long WorldSpace);
 long TestCollision(ITEM_INFO* item, ITEM_INFO* l);
 void InitInterpolate2(long frac, long rate);
@@ -10,10 +16,5 @@ void GetJointAbsPosition(ITEM_INFO* item, PHD_VECTOR* pos, long joint);
 
 extern SPHERE Slist[34];
 extern char GotLaraSpheres;
-typedef struct SPHERE {
-	long x;
-	long y;
-	long z;
-	long r;
-} SPHERE;
-#endif
+
+#endif// TOMB4_GAME_SPHERE_H
