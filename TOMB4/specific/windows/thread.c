c01d2135f1c0cec4b94a0a8d4e51c2e0dc548519
Raildex <n@a.com>
2024-08-07 22:13:31 +0200

    Fixed Thread payload

--- TOMB4/specific/windows/thread.c
+++ TOMB4/specific/windows/thread.c
@@ -13,24 +13,27 @@ typedef struct WIN32PAYLOAD {
 } WIN32PAYLOAD;
 
 ULONG WINAPI Win32ThreadFunc(void* arg) {
-	WIN32PAYLOAD payload = *(WIN32PAYLOAD*)(arg);
-	int status = payload.func(payload.func);
+	WIN32PAYLOAD* payload = (WIN32PAYLOAD*)(arg);
+	int status = payload->func(payload->arg);
+	free(payload);
 	return status;
 }
 
 long S_CreateThread(THREAD** output, THREADFUNC func, void* arg) {
 	THREAD* thrd = calloc(1, sizeof(THREAD));
-	WIN32PAYLOAD payload;
-	payload.arg = arg;
-	payload.func = func;
-	HANDLE handle = CreateThread(NULL, 0, Win32ThreadFunc, &payload, CREATE_SUSPENDED, &thrd->Win32Handle);
-	if(handle) {
-		thrd->ThreadHandle = handle;
-		*output = thrd;
-		return 1;
+	WIN32PAYLOAD* payload = calloc(1, sizeof(WIN32PAYLOAD));
+	payload->arg = arg;
+	payload->func = func;
+	HANDLE handle = CreateThread(NULL, 0, Win32ThreadFunc, payload, CREATE_SUSPENDED, &thrd->Win32Handle);
+	if(!handle) {
+		free(payload);
+		*output = NULL;
+		return 0;
 	}
-	*output = NULL;
-	return 0;
+	thrd->ThreadHandle = handle;
+	*output = thrd;
+	return 1;
+
 }
 
 long S_RunThread(THREAD* thrd) {
